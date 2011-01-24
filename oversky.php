3a63049a1e44adffdfc1030eab8964e90cbbb8eb
Carlo Alberto Ferraris <cafxx@strayorange.com>
2011-01-24 14:39:36 -0800

    Initial commit

---
+++ oversky.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+    ///////////////////////////////////////////
+    //    __        ___  __   __             //
+    //   /  \ \  / |__  |__) /__` |__/ \ /   //
+    //   \__/  \/  |___ |  \ .__/ |  \  |    //
+    //                                       //
+    ///////////////////////////////////////////
+
+             the sky is not the limit
+
+2011 Carlo Alberto Ferraris <cafxx@strayorange.com>
+                                                    */
+
+require_once 'oversky.phpi';
+require_once 'oversky.constants.phpi';
+
+liftOff(); // let's roll
+
+?>
